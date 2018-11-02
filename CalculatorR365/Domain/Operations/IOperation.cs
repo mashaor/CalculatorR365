36fa05c57e9e3922d788cb83f3fd22e61e495341
Masha Mortov <masha_mortov@hotmail.com>
2018-11-02 14:59:24 -0700

    refactor operations as interface

---
+++ CalculatorR365/Domain/Operations/IOperation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Domain.Enums;
+
+namespace Domain.Operations
+{
+    interface IOperation
+    {
+        int Calculate(List<int> input);
+
+        bool IsMatch(SupportedOperations operation);
+        
+    }
+}
---
+++ CalculatorR365/Domain/Operations/Operations.cs
@@ -0,0 +1,94 @@
+﻿using Domain.Enums;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Domain.Operations
+{
+    public class Add : IOperation
+    {
+        public int Calculate(List<int> input)
+        {
+            int result = 0;
+
+            if (input != null)
+            {
+                result = input.Sum();
+            }
+
+            return result;
+        }
+
+        public bool IsMatch(SupportedOperations operation)
+        {
+            return SupportedOperations.Add == operation;
+        }
+    }
+
+    public class Multiply : IOperation
+    {
+        public int Calculate(List<int> input)
+        {
+            int result = 0;
+
+            if (input != null)
+            {
+                result = input.Aggregate(1, (a, b) => a * b);
+            }
+
+            return result;
+        }
+
+        public bool IsMatch(SupportedOperations operation)
+        {
+            return SupportedOperations.Multiply == operation;
+        }
+    }
+
+    public class Divide : IOperation
+    {
+        public int Calculate(List<int> input)
+        {
+            int result = 0;
+
+            if (input != null && input.Count > 0)
+            {
+                result = input[0];
+
+                for (int i = 1; i < input.Count; i++)
+                {
+                    if (input[i] != 0)
+                    {
+                        result /= input[i];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public bool IsMatch(SupportedOperations operation)
+        {
+            return SupportedOperations.Divide == operation;
+        }
+    }
+
+    public class Subtract : IOperation
+    {
+        public int Calculate(List<int> input)
+        {
+            int result = input[0];
+
+            for (int i = 1; i < input.Count; i++)
+            {
+                result -= input[i];
+            }
+
+            return result;
+        }
+
+        public bool IsMatch(SupportedOperations operation)
+        {
+            return SupportedOperations.Subtract == operation;
+        }
+    }
+}
