df4ce91d76069024a87bef17cce07df6af7d23a6
Masha Mortov <masha_mortov@hotmail.com>
2018-11-02 15:16:07 -0700

    refactor rules as interface

---
+++ CalculatorR365/Domain/Rules/Rules.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Rules
+{
+    public class IgnoreNegatives : IRule
+    {
+        public List<int> Apply(List<int> input)
+        {
+            //check for any negative numbers
+            if (input.Any(n => n < 0))
+            {
+                var allNegatives = String.Join(", ", input.Where(n => n < 0));
+                throw new Exception(string.Format("Negatives are not allowed: {0}", allNegatives));
+            }
+
+            return input;
+        }
+    }
+
+    public class ExcludeLargeNumbers : IRule
+    {
+        public List<int> Apply(List<int> input)
+        {
+            return input.Where(n => n < 1000).ToList();
+        }
+    }
+}
